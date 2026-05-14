3a758260ffd16f3eb73ebdb524492ec1cb43b82e
shine7658 <shine941011@gmail.com>
2026-05-14 11:08:33 +0800

    webhook 1

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, jsonify
 from datetime import datetime
 
 import os
@@ -45,6 +45,16 @@ def index():
     link += "<a href=/rate>本週新片進DB</a><hr>"
 
     return link
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
+    return make_response(jsonify({"fulfillmentText": info}))
 
 @app.route("/rate")
 def rate():
